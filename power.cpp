68b9762df1c1a00ca92fc99b6ee2e219a346afb6
Gohar Ayub <goharayub260@gmail.com>
2025-11-01 16:50:14 +0500

    Power exponent using recursion with both (Slower/fast) Efficiency

---
+++ power.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+int  pow(int m,int n)
+{
+    if(n==0)
+        return 1;
+    else
+       return pow(m,n-1)*m;
+}
+
+int fastPow(int m,int n)
+{
+    if(n==0)
+        return 1;
+    if(n%2==0)
+        return fastPow(m*m,n/2);
+    else 
+        return m*fastPow(m*m,(n-1)/2);
+
+}
+
+int main()
+{
+    int base,exponent,choice;
+    cout<<"Enter the base: "<<endl;
+    cin>>base;
+    cout<<"Enter exponent: "<<endl;
+    cin>>exponent;
+    cout<<"\nChoose method\n"<<"1. simple recursion (more time complexity O(n))\n"<<"2. Fast exponentiation (less time complexity O(log n))\n"<<endl;
+    cin>>choice;
+    if(choice==1)
+    {
+        printf("%d O(n) \n",pow(base,exponent));
+    }
+    else 
+    {
+        printf("%d O(log n) \n",fastPow(base,exponent)); 
+    }
+
+}
